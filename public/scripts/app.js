e6e766d702faf48f9f5ee0dc809b3fbd8a0bb5c7
Dong Hu <cdhu@uwaterloo.ca>
2017-05-17 14:48:01 +0000

    Implemented .preventDefault and .serialize upon form submission

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -5,22 +5,7 @@
  */
 
  // TEST
- var tweetData = {
-   "user": {
-     "name": "Newton",
-     "avatars": {
-       "small":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_50.png",
-       "regular": "https://vanillicon.com/788e533873e80d2002fa14e1412b4188.png",
-       "large":   "https://vanillicon.com/788e533873e80d2002fa14e1412b4188_200.png"
-     },
-     "handle": "@SirIsaac"
-   },
-   "content": {
-     "text": "If I have seen further it is by standing on the shoulders of giants"
-   },
-   "created_at": 1461116232227
- }
-
+ $(document).ready(function () {
  var data = [
    {
      "user": {
@@ -98,15 +83,21 @@ function renderTweets(tweets) {
   for (tweet in tweets) {
     let tweetObject = tweets[tweet];
     let $tweet = createTweetElement(tweetObject);
-    console.log($('#tweets-container'))
     $('#tweets-container').prepend($tweet);
   }
 }
 
-// run the jquery
-$(document).ready(function () {
-  renderTweets(data);
+renderTweets(data);
 
+// prevents redirection after submission of form, serializes
+// input into a jquery string
+$('#tweet-box').on('submit', function (event) {
+  event.preventDefault();
+  var $enteredTweet = $(this).find('textarea').serialize();
+  console.log($enteredTweet);
+});
+
+// run the jquery
   // $('#tweet-box').on('submit', function (e) {
   //   e.preventDefault();
   //   console.log($('textarea').val());
