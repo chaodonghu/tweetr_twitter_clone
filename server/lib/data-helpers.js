0dce42bb193c5af5be5ccf53914889c029e65a4e
Dong Hu <cdhu@uwaterloo.ca>
2017-05-18 19:10:19 +0000

    Converted to MongoDB

--- server/lib/data-helpers.js
+++ server/lib/data-helpers.js
@@ -10,18 +10,23 @@ module.exports = function makeDataHelpers(db) {
     // Saves a tweet to `db`
     saveTweet: function(newTweet, callback) {
       simulateDelay(() => {
-        db.tweets.push(newTweet);
+        db.collection("tweets").insertOne(newTweet);
         callback(null, true);
       });
     },
 
     // Get all tweets in `db`, sorted by newest first
     getTweets: function(callback) {
-      simulateDelay(() => {
-        const sortNewestFirst = (a, b) => a.created_at - b.created_at;
-        callback(null, db.tweets.sort(sortNewestFirst));
+      console.log("getTweets callback");
+      db.collection("tweets").find().toArray((err, tweets) => {
+        if (err) {
+          return callback(err);
+        }
+        console.log("callback function is fired");
+        callback(null, tweets);
+        console.log("tweets", tweets);
+        console.log('end of tweets function');
       });
     }
-
-  };
+  }
 }
